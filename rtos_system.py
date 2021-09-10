20220f0d00f40124a5122a2dedec264fdcfb97bf
Rodrigo Azpilcueta <ayasms@gmail.com>
2021-09-10 17:11:27 -0300

    Agregado de comentarios y correción de pequeños erros

--- rtos_system.py
+++ rtos_system.py
@@ -1,45 +1,79 @@
 from math import ceil
 from numpy import lcm
+from nptyping import Int32
 
 class Rate_Monotonic():
 
-    def __init__(self, tasks : list = []) -> None:        
+    def __init__(self, tasks : list = []) -> None: 
+        '''
+        Inicializa el sistema con el conjunto de tareas
+        ingresadas como argumento
+        '''       
         self._tasks = tasks
 
     def add_task(self, task):
+        '''
+        Agrega tareas al sistema
+        '''
         self._tasks.append(task)
 
-    def hyperperiod(self):
+    def hyperperiod(self) -> Int32:
+        '''
+        Calcula el hiperperiodo del sistema buscando
+        el mínimo común múltiplo entre todos los períodos
+        de las tareas del sistema.
+        '''
         return lcm.reduce([x.period for x in self._tasks])
 
-    def utilization_factor(self) -> float:        
-        u = 0
+    def utilization_factor(self) -> float:
+        '''
+        Calcula el factor de utilización del sistema.
+        '''        
+        u = 0 # Factor de utilización
         for task in self._tasks:
-            u += task.ex_time / task.period
-        return round(u, 15)
+            u += task.ex_time / task.period # Ci / Ti
+        return round(u, 15) # Redonde para mejorar visualización
 
-    def liu_layland(self) -> list:                
-        n = len(self._tasks)
-        test_condition = n * (2 ** (1/n) - 1)
-        return [self.utilization_factor() <= test_condition, test_condition]
+    def liu_layland(self) -> list: 
+        '''
+        Cálculo de la cota de Liu-Layland para el sistema. Devuelve una lista de dos elementos
+        el primer elemento True si cumple la cota, False de lo contrario. El segundo elemento devuelve
+        el valor numerico resultante del calculo de la cota.
+        '''               
+        n = len(self._tasks) # Número de tareas
+        test_condition = n * (2 ** (1/n) - 1) # Cálculo de la cota
+        return [self.utilization_factor() <= test_condition, test_condition] # (Condición de la cota, cálculo)
 
-    def bini(self) -> list:        
-        u = 1
+    def bini(self) -> list:
+        '''
+        Cálculo de la cota de Bini para el sistema. Devuelve una lista de dos elementos
+        el primer elemento True si cumple la cota, False de lo contrario. El segundo elemento devuelve
+        el valor numerico resultante del calculo de la cota.               
+        '''        
+        u = 1 # Inicializo con 1 para la productoria
         for task in self._tasks:
-            u *= (task.ex_time / task.period) + 1
-        return [u <= 2, u]
+            u *= (task.ex_time / task.period) + 1 
+        return [u <= 2, u] 
 
-    def joshep_pandya(self, verbose : bool=False) -> None:
-        n_iteration = 0        
+    def joshep_pandya(self, verbose : bool=False) -> bool:
+        '''
+        Cálculo del Test de planificabilidad de M. Joseph y P. Pandya.
+        Imprime en pantalla el peor tiempo de respuesta de cada tarea. 
+        Devuelve True si es planificable, False de lo contrario.
+        Si se especifica verbose = True imprime en pantalla el número de 
+        iteraciones y funciones techo utilizadas.
+        '''
+        n_iteration = 0 # Contador de iteraciones
+        ceil_count = 0  # Contador de funciones techo.       
         for n, task in enumerate(self._tasks):
-            tq = 0
-            ceil_count = 0            
-            print('Task:',n+1)
+            tq = 0 # Tiempo actual
+                       
+            print('Task:',n+1) 
             if n == 0:
                 print('PF:',task.ex_time)
             else:
-                while tq <= task.deadline:
-                    cumsum = 0
+                while tq <= task.deadline: 
+                    cumsum = 0 # Valor de las sumatorias con funciones techo
                     n_iteration += 1                 
                     for i in range(n):
                         ceil_count += 1
@@ -51,14 +85,24 @@ class Rate_Monotonic():
                         tq = task.ex_time + cumsum
                 if tq > task.deadline:
                     print('No es programable  Numero de iteraciones:')
+                    return False
         if verbose:
             print('Número de iteraciones:', n_iteration)
             print('Numero de funciones techo:', ceil_count)
+        return True
 
-    def rta(self, verbose : bool=False) -> None:
-        n_iteration = 0
-        tq = 0
-        ceil_count = 0        
+    def rta(self, verbose : bool=False) -> bool:
+        '''
+        Cálculo del Test de planificabilidad de por RTA
+        Imprime en pantalla el peor tiempo de respuesta de cada tarea. 
+        Devuelve True si es planificable, False de lo contrario.
+        Si se especifica verbose = True imprime en pantalla el número de 
+        iteraciones y funciones techo utilizadas.
+        '''
+        n_iteration = 0 # Contador de iteraciones
+        ceil_count = 0 #Contador de funciones techo
+        tq = 0 # Tiempo actual
+                 
         for n, task in enumerate(self._tasks):                        
             print('Task:',n+1)
             if n == 0:
@@ -67,7 +111,7 @@ class Rate_Monotonic():
             else:
                 tq += task.ex_time
                 while tq <= task.deadline:
-                    cumsum = 0
+                    cumsum = 0 # Valor de las sumatorias con funciones techo
                     n_iteration += 1                 
                     for i in range(n):
                         ceil_count += 1
@@ -83,13 +127,22 @@ class Rate_Monotonic():
         if verbose:
             print('Número de iteraciones:', n_iteration)
             print('Numero de funciones techo:', ceil_count)
+        return True
 
     def rta2(self, verbose : bool=False) -> None:
-        n_iteration = 0
-        tq = 0
-        ceil_count = 0                         
+        '''
+        Cálculo del Test de planificabilidad de por RTA 2
+        Imprime en pantalla el peor tiempo de respuesta de cada tarea. 
+        Devuelve True si es planificable, False de lo contrario.
+        Si se especifica verbose = True imprime en pantalla el número de 
+        iteraciones y funciones techo utilizadas.
+        '''
+        n_iteration = 0 # Contador de iteraciones
+        ceil_count = 0 #Contador de funciones techo
+        tq = 0 # Tiempo actual
+        a_coeff = [task.ex_time for task in self._tasks]                                
         for n, task in enumerate(self._tasks):
-            a_coeff = [task.ex_time for task in self._tasks]                         
+                                    
             print('Task:',n+1)
             if n == 0:
                 tq = task.ex_time
@@ -117,6 +170,7 @@ class Rate_Monotonic():
         if verbose:
             print('Número de iteraciones:', n_iteration)
             print('Numero de funciones techo:', ceil_count) 
+        return True
 
     def rta3(self, verbose : bool=False) -> None:
         n_iteration = 0
@@ -161,6 +215,60 @@ class Rate_Monotonic():
     
         return lcm.reduce([x.period for x in self._tasks])
     
+    def empty_slot(self, slot_size : int, verbose : bool = False) -> int:
+        '''
+        Devuelve el instante de tiempo donde se encuentra
+        una ranura vacia de tamaño 'slot_size'.
+        '''        
+        a_coeff = [task.ex_time for task in self._tasks]
+        t_valid = [-1 for task in self._tasks]
+        tq = slot_size
+        while True:
+            cumsum = 0
+            for n, task in enumerate(self._tasks):
+                if tq > t_valid[i]:
+                    ceil_calc = ceil(tq / task.period)
+                    a = ceil_calc * task.ex_time
+                    cumsum += a
+                    t_valid[i] = ceil_calc * task.period
+                    if a_coeff[i] < a:
+                        a_coeff[i] = a
+                        tq += a - a_coeff[n]
+        # for n, task in enumerate(self._tasks):
+        #     a_coeff = [task.ex_time for task in self._tasks]
+        #     t_valid = [-1 for task in self._tasks]                         
+        #     print('Task:',n+1)
+        #     if n == 0:
+                tq = task.ex_time
+                print('PF:',task.ex_time)
+            else:
+                tq += task.ex_time
+                while tq <= task.deadline:
+                    cumsum = 0
+                    n_iteration += 1                 
+                    for i in range(n):
+                        if tq > t_valid[i]:
+                            ceil_count += 1
+                            ceil_calc = ceil(tq / self._tasks[i].period)
+                            a =  ceil_calc * self._tasks[i].ex_time
+                            cumsum += a
+                            t_valid[i] = ceil_calc * self._tasks[i].period
+                            if a_coeff[i] < a:                            
+                                a_coeff[i] = a                            
+                                tq += a - a_coeff[n] 
+                        else:
+                            cumsum += a_coeff[i]
+                                                           
+                    if tq == task.ex_time + cumsum:                        
+                        print('PF:',tq)                                                            
+                        break
+                    else:                    
+                        tq = task.ex_time + cumsum
+                if tq > task.deadline:
+                    print('No es programable')
+                    return False
+
+        
 
 class Task():
 
@@ -194,7 +302,8 @@ class Task():
         self._deadline = deadline 
     
     
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    # TP1    
     ex_tasks = [[Task(1,3,3), Task(1,4,4), Task(1,6,6)],
                 [Task(2,4,4), Task(1,5,5), Task(1,8,8)],
                 [Task(1,4,4), Task(1,5,5), Task(2,7,7), Task(1,12,12)],
@@ -209,6 +318,7 @@ if __name__ == '__main__':
         print('Factor de utilización:', system.utilization_factor())
         print('Cota de Liu:', system.liu_layland()[1])
         print('Cota de Bini:', system.bini()[1])    
-        system.rta3(verbose=True)
+        system.rta2(verbose=True)
+        
         
 
